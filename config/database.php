77262a6a534701cb52411e2f964e243ab5238e74
MD. SHABUDDIN <86931137+Nayan538@users.noreply.github.com>
2022-09-18 18:05:46 +0600

    config

---
+++ config/database.php
@@ -0,0 +1,6 @@
+<?php
+$GLOBALS['DBHOST'] = "localhost";
+$GLOBALS['DBNAME'] = "sales_and_inventory";
+$GLOBALS['DBUSER'] = "root";
+$GLOBALS['DBPASS'] = "";
+?>
---
+++ config/site.php
@@ -0,0 +1,9 @@
+<?php 
+#### WEBSITE CONFIGURATIONS ####
+$GLOBALS['CURRENCY'] = "&#2547;" ;
+
+#### UPLOAD DIRECTORIES ####
+# Slider Image
+$GLOBALS['PRODUCT_DIRECTORY'] = "../secureadmin/public/uploads/images/products/";
+$GLOBALS['ADMINS_DIRECTORY'] = "../secureadmin/public/uploads/images/admins/";
+?>
